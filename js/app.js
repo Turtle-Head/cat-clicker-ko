f35264f987f813cb7e6e0a331310a16b478cd242
Turtle-Head <djinn.jecture@gmail.com>
2015-12-04 11:33:20 -0600

    Added some data bindings for functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 var ViewModel = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('http://www.medhatspca.ca/sites/default/files/news_photos/2014-Apr-15/node-147/cute-little-cat.jpg');
+  this.imgSrc = ko.observable('img/cute-little-cat.jpg');
   this.imgAttribution = ko.observable('http://www.medhatspca.ca/sites/default/files/news_photos/2014-Apr-15/node-147/cute-little-cat.jpg');
+
+  this.incrementCounter = function(){
+    this.clickCount(this.clickCount()+1);
+  };
 };
 
 ko.applyBindings(new ViewModel());
